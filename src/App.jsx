1d4e2a553341a24a59ffc65896dfb5b7bfe4ed79
176409 <148116817+176409@users.noreply.github.com>
2026-09-16 22:38:39 +0600

    second push

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="h-screen flex  justify-center items-center">
+      <h1 className="text-9xl text-red-500 text-center">
+        Hi I am Masood Hossain  {" "}
+      </h1>
+    </div>
+  );
+};
+
+export default App;
